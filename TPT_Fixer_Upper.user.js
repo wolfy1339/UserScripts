6b8aca93f557511978d0452d204ce4eec05b0801
wolfy1339 <wolfy1339@users.noreply.github.com>
2014-08-01 11:56:43 -0400

    Update TPT_Fixer_Upper.user.js

--- TPT_Fixer_Upper.user.js
+++ TPT_Fixer_Upper.user.js
@@ -14,4 +14,4 @@
 $(".social-github iframe").attr('src', "http://ghbtns.com/github-btn.html?user=simtr&repo=The-Powder-Toy&type=watch&count=true");
 //Make Groups system better
 $(".Meta .author img").css({"z-index":"-1", "border-radius":"3px", "position":"relative", "box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-moz-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-webkit-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-o-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-ms-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "background":"-webkit-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%)", "background":"-o-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%)", "background":"-ms-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%)"});
-$(".Pageheader").removeClass("Pageheader").addClass("breadcrumb");
+$(".Pageheader").removeClass("Pageheader").addClass("breadcrumb").css({"margin":"0"});
