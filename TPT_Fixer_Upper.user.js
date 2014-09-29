4fa80337f953ea71073aa4e2bf8208e4b1624409
wolfy1339 <wolfy1339@users.noreply.github.com>
2014-09-29 12:54:24 -0400

    Major Update!!

--- TPT_Fixer_Upper.user.js
+++ TPT_Fixer_Upper.user.js
@@ -6,31 +6,31 @@
 // @copyright   2014+, wolfy1339
 // @license     GNU GPLv3
 // @downloadURL https://openuserjs.org/install/wolfy1339/TPT_Fixer_Upper.user.js
-// @version     1.41
+// @version     1.42
 // @grant       none
 // @include     http*://powdertoy.co.uk/*
 // ==/UserScript==
 
 //Fix GitHub watch button
-$(".social-github iframe").attr('src', "http://ghbtns.com/github-btn.html?user=simtr&repo=The-Powder-Toy&type=watch&count=true");
+jQuery(".social-github iframe").attr('src', "http://ghbtns.com/github-btn.html?user=simtr&repo=The-Powder-Toy&type=watch&count=true");
 //Fixes for the rebuilt search feature
-$(".search-avatar").css({"margin-right":"10px"});
-$(".search-thumbnail img").css({"border-radius":"3px", "border":"2px solid #DDD"});
-$(".search-result .details").css({"margin-left":"70px", "margin-right":"20px"});
-$(".posts .search-thumbnail").css({"width":"63px"});
-$(".threads .search-thumbnail").css({"width":"63px"});
+jQuery(".search-avatar").css({"margin-right":"10px"});
+jQuery(".search-thumbnail img").css({"border-radius":"3px", "border":"2px solid #DDD"});
+jQuery(".search-result .details").css({"margin-left":"70px", "margin-right":"20px"});
+jQuery(".posts .search-thumbnail").css({"width":"63px"});
+jQuery(".threads .search-thumbnail").css({"width":"63px"});
 //Fix, if number is big it won't overflow as much
-$(".TopicList li .Meta span").css({"max-height":"14px", "font-size":"10px"});
+jQuery(".TopicList li .Meta span").css({"max-height":"14px", "font-size":"10px"});
 //Make Groups system better
 if (window.location.toString().indexOf("/Groups/")!=-1){
-	$(".Pageheader").addClass("breadcrumb").css({"margin":"0","border-top":"none", "border-right":"none", "border-left":"none"}).removeClass("Pageheader");
-	$("head").append("<style>.Meta .Author img{background: linear-gradient(to top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%); background: -webkit-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%); background: -o-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%);background: -ms-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%);}")
-	$(".Meta .Author img").css({"z-index":"-1", "border-radius":"3px", "box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-moz-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-webkit-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-o-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-ms-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)"});
-	$('.Message span[style="color: white;"]').removeAttr("style");
+	jQuery(".Pageheader").addClass("breadcrumb").css({"margin":"0","border-top":"none", "border-right":"none", "border-left":"none"}).removeClass("Pageheader");
+	jQuery("head").append("<style>.Meta .Author img{background: linear-gradient(to top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%); background: -webkit-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%); background: -o-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%);background: -ms-linear-gradient(top, rgba(255,255,255,0.1) 0%,rgba(0,0,0,0.1) 100%);}")
+	jQuery(".Meta .Author img").css({"z-index":"-1", "border-radius":"3px", "box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-moz-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-webkit-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-o-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)", "-ms-box-shadow":"0 0 5px rgba(0, 0, 0, 0.25), inset 0 1px 0 rgba(255, 255, 255, 0.4)"});
+	jQuery('.Message span[style="color: white;"]').removeAttr("style");
 }
 if (window.location.toString().indexOf("/Groups/Page/Index.html")!=-1){
-	$(".breadcrumb").removeClass("breadcrumb").removeClass("Submenu").addClass("Pageheader").addClass("Submenu");
-	$(".Page").css({"border":"none"});
-	$(".contents").css({"padding":"10px","background":"white", "border":"1px solid #DDD"});
-	$(".GroupItem:last-child").css({"border-bottom":"none"});
+	jQuery(".breadcrumb").removeClass("breadcrumb").removeClass("Submenu").addClass("Pageheader").addClass("Submenu");
+	jQuery(".Page").css({"border":"none"});
+	jQuery(".contents").css({"padding":"10px","background":"white", "border":"1px solid #DDD"});
+	jQuery(".GroupItem:last-child").css({"border-bottom":"none"});
 }
