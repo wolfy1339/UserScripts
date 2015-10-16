e7d5eb8db96e390c4b0c19df9df96686e0e52879
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-10-16 00:36:48 -0400

    Remove numFiles

--- TPT_Fixer_Upper.user.js
+++ TPT_Fixer_Upper.user.js
@@ -153,14 +153,13 @@ var TPTFixerUpper = function() {
 
         jQuery(document).on("change", ".btn-file :file", function() {
             var input = jQuery(this);
-            var numFiles = input.get(0).files ? input.get(0).files.length : 1;
             var label = input.val().replace(/\\/g, "/").replace(/.*\//, "");
-            input.trigger("fileselect", [numFiles, label]);
+            input.trigger("fileselect", label);
         });
 
-        jQuery(".btn-file :file").on("fileselect", function(event, numFiles, label) {
+        jQuery(".btn-file :file").on("fileselect", function(event, label) {
             var input = jQuery(this).parents(".input-prepend").find(":text");
-            var log = numFiles > 1 ? numFiles + " files selected" : label;
+            var log = label;
 
             if (input.length) {
                 input.val(log);
