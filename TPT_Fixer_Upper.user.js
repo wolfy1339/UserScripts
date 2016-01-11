3e156bd06def7fbdcc43852505c27c61904e48db
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-01-10 23:27:36 -0500

    Bump version

--- TPT_Fixer_Upper.user.js
+++ TPT_Fixer_Upper.user.js
@@ -8,7 +8,7 @@
 // @copyright   2014-2015, wolfy1339
 // @license     MIT License
 // @downloadURL https://openuserjs.org/install/wolfy1339/TPT_Fixer_Upper.user.js
-// @version     2.10
+// @version     2.11
 // @grant       none
 // @match       *://powdertoy.co.uk/*
 // ==/UserScript==
