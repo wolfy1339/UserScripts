458f4546792da179fa8b5e85d131ba82cf0cd2cc
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-05-15 16:32:12 -0400

    Re-indent check_trailing_whitespaces.py to 4 spaces

--- check_trailing_whitespaces.py
+++ check_trailing_whitespaces.py
@@ -15,31 +15,31 @@ import re
 import sys
 
 def has_trailing_whitespaces(path):
-  """ Returns whether |path| has trailing whitespaces. """
-  handle = open(path, 'r')
-  for line in handle:
+    """ Returns whether |path| has trailing whitespaces. """
+    handle = open(path, 'r')
+    for line in handle:
     for idx in range(-1, -len(line) - 1, -1):
-      if line[idx] in ('\n', '\r'):
-        continue
-      if line[idx] in (' ', '\t'):
-        handle.close()
-        return True
-      break
-  handle.close()
+        if line[idx] in ('\n', '\r'):
+            continue
+        if line[idx] in (' ', '\t'):
+            handle.close()
+            return True
+        break
+    handle.close()
   return False
 
 if len(sys.argv) > 2:
-  sys.exit('Usage: ' + sys.argv[0] + ' [root]')
+    sys.exit('Usage: ' + sys.argv[0] + ' [root]')
 
 top = os.getcwd()
 if len(sys.argv) == 2:
-  top += '/' + sys.argv[1]
+    top += '/' + sys.argv[1]
 
 for root, dirs, files in os.walk(top):
-  for f in files:
-    if f.endswith('.py') or f.endswith('.user.js') or f.endswith('.travis.yml') or f.endswith('.md'):
-      path = root + '/' + f
-      if has_trailing_whitespaces(path):
-        sys.exit('Found a file with trailing whitespaces: ' + path)
+    for f in files:
+        if f.endswith('.py') or f.endswith('.user.js') or f.endswith('.travis.yml') or f.endswith('.md'):
+            path = root + '/' + f
+            if has_trailing_whitespaces(path):
+                sys.exit('Found a file with trailing whitespaces: ' + path)
 
 sys.exit(0)
